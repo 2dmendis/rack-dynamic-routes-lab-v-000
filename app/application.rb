767fcc05fd43ddfd111cbafabea70323aae45377
Dmitri Mendis <2dmendis@gmail.com>
2018-06-19 05:20:13 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application 
+  
+  @@items = []
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if  @@items.find{|s| s.name == item_name}
+        name = @@items.find{|s| s.name == item_name}
+        resp.write name.price
+      else 
+        resp.write "Item was not found"
+        resp.status = 400
+      end 
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+  end 
+end 
